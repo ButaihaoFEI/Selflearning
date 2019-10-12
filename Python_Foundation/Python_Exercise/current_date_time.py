4016921659ec15ebd6f8add7913c819c5df8f99e
FEI Shenghao <feishenghao@gmail.com>
2019-10-12 16:11:22 +0800

    reorganise the catalogue

--- Python_Foundation/Python_Exercise/current_date_time.py
+++
@@ -1,4 +0,0 @@
-import datetime
-now = datetime.datetime.now()
-print(now)
-print(now.strftime("%Y-%m-%d %H:%M:%S"))
--- Python_Foundation/Python_Exercise/sum_list.py
+++
@@ -1,4 +0,0 @@
-def sum_list(input):
-    return sum(input)
-
-print(sum_list([1,2,3]))
